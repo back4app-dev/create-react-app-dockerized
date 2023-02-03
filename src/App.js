d62c316862daa10bdd0e7c8fa00108b0948d1b4e
sadakchap <aliceprerna@gmail.com>
2023-02-03 20:33:19 +0530

    cleanup & add blog thing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setPosts(data);
+      }      
+    } catch (err) {
+      console.log('request failed')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      My blog 
+      <div className="">
+        {posts.map((post) => {
+          return <pre key={post.id}>{JSON.stringify(post)}</pre>
+        })}
+      </div>
     </div>
   );
 }
